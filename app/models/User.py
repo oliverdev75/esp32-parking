b83276536fe528fb3adb99c65f177a8fbee0918d
Oliver Ros <seair75@gmail.com>
2025-01-14 00:32:00 +0100

    Base almost done. User doesn't serialize!

--- app/models/User.py
+++ app/models/User.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Integer, String, DateTime
-from .. import db
+from sqlalchemy import Integer, String, DateTime, ForeignKey, text
+from sqlalchemy.orm import declared_attr, relationship
+from .. import app, db
+from .Role import Role
 from datetime import datetime
-import bcrypt
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -12,25 +13,12 @@ class User(db.Model):
     password = db.Column(String(255), nullable=False)
     fullname = db.Column(String(255), nullable=False)
     contact = db.Column(Integer, nullable=False)
-    role = db.Column(String, nullable=False)
+    role_id = db.Column(String, ForeignKey(Role.id), nullable=False)
 
-    def __init__(
-        self,
-        email,
-        password,
-        fullname,
-        surnames,
-        contact,
-        role,
-    ):
-        super().__init__()
-        self.email = email
-        self.fullname = fullname
-        self.surnames = surnames
-        self.contact = contact
-        self.role = role
-        self.created_at = datetime.now()
-        self.password = bcrypt.hashpw(
-            password.encode('utf-8'),
-            bcrypt.gensalt()
-        )
+    """ @declared_attr
+    def role(self):
+        role_name = None
+        query = text(f"SELECT * FROM roles WHERE id = :role")
+        with app.app_context():
+            role_name = db.session.execute(query, params=dict(role=self.role_id))
+        return role_name """
